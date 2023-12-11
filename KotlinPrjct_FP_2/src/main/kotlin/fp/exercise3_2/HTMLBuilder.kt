bf5e49f59483816d7d86d21f16d11affd6b09982
Simon Primetzhofer <simon.primetzhofer@unriskomega.com>
2023-12-11 18:08:04 +0100

    complete ex3_2 implementation

--- KotlinPrjct_FP_2/src/main/kotlin/fp/exercise3_2/HTMLBuilder.kt
+++ KotlinPrjct_FP_2/src/main/kotlin/fp/exercise3_2/HTMLBuilder.kt
@@ -1,32 +1,84 @@
 package fp.exercise3_2
 
-fun page(init: PageBuilder.() -> Unit) : Page {
-    val b : PageBuilder = PageBuilder()
+import java.io.File
+
+fun page(init: PageBuilder.() -> Unit): Page {
+    val b = PageBuilder()
     b.init()
     return Page(b.title, b.elements)
 }
 
 abstract class ContainerBuilder<E> {
-    internal val elements : MutableList<E> = mutableListOf()
+    internal val elements: MutableList<E> = mutableListOf()
 }
 
 open class ElementContainerBuilder : ContainerBuilder<Element>() {
+    fun heading(level: Int, text: String): Heading {
+        val heading = Heading(level, text)
+        elements.add(heading)
+        return heading
+    }
+
+    fun text(text: String): Text {
+        val textElement = Text(text)
+        elements.add(textElement)
+        return textElement
+    }
+
+    fun paragraph(init: ParagraphBuilder.() -> Unit): Paragraph {
+        val b = ParagraphBuilder()
+        b.init()
+        // I decided that if multiple lines are within a paragraph, they are only separated by a whitespace (and not a newline for example)
+        val paragraph = Paragraph(b.elements.joinToString(" ") { it })
+        elements.add(paragraph)
+
+        return paragraph
+    }
+
+    fun div(init: ElementContainerBuilder.() -> Unit): Div {
+        val b = ElementContainerBuilder()
+        b.init()
+        val div = Div(b.elements)
+
+        elements.add(div)
+
+        return div
+    }
+
+    fun list(ordered: Boolean, init: ListBuilder.() -> Unit): HTMLList {
+        val b = ListBuilder()
+        b.init()
+        val htmlList = HTMLList(b.elements, ordered)
+
+        elements.add(htmlList)
+
+        return htmlList
+    }
 }
 
 class PageBuilder : ElementContainerBuilder() {
-    var title : String = ""
+    var title: String = ""
 }
 
 class ListBuilder : ContainerBuilder<ListItem>() {
+    fun item(init: ElementContainerBuilder.() -> Unit): ListItem {
+        val b = ElementContainerBuilder()
+        b.init()
+        val listItem = ListItem(b.elements)
+
+        elements.add(listItem)
+
+        return listItem
     }
+}
 
 class ParagraphBuilder : ContainerBuilder<String>() {
+    fun line(text: String) = elements.add(text)
 
+    operator fun String.unaryPlus() = line(this)
 }
 
 fun main() {
-    /*
-
     val page =
         page {
             title = "My Page"
@@ -34,6 +86,7 @@ fun main() {
             div {
                 paragraph {
                     +"Kotlin is"
+                    +"cool and a" // added this line for testing multiple lines within a paragraph
                 }
                 list(true) {
                     item {
@@ -94,6 +147,4 @@ fun main() {
 
         }
     File("page2.html").writeText(HTMLRenderer.render(page))
-
-     */
 }
